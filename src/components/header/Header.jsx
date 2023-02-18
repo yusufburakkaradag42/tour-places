265c160026b255503c866e777d0a91b505cae2cd
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-02-18 18:01:27 +0300

    end

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Card from '../cards/Card'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <h1>POPULER TOUR PLACES</h1>
+       
+             <Card/>
+      
+       
+
+    </div>
+    
+  )
+}
+
+export default Header
